5faac0bda08da555a5c35ef46a236dc9b16db6fc
kumarisonali21 <sonalisingh2921@gmail.com>
2026-05-04 15:53:16 +0530

    added root route

--- backend/server.js
+++ backend/server.js
@@ -309,6 +309,10 @@ cron.schedule('0 9 * * *', async () => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running ");
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("Server running on port " , PORT);
